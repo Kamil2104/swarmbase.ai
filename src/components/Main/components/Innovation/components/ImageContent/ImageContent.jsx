4da439daa265e0b4c9d2bf00a0e88f5735faf037
pindrive <kamilniewiadomski18@gmail.com>
2024-08-24 19:49:51 +0200

    feat: add icon for first picture in Innovation component, also make style for it

--- src/components/Main/components/Innovation/components/ImageContent/ImageContent.jsx
+++ src/components/Main/components/Innovation/components/ImageContent/ImageContent.jsx
@@ -1,5 +1,6 @@
 import Image from './components/Image';
 
+import ComputerHeadIcon from './images/ComputerHeadIcon.png';
 import Robot1 from './images/Robot1.jpg'
 import Robot2 from './images/Robot2.png'
 
@@ -8,8 +9,8 @@ import './styles/ImageContent.css';
 const ImageContent = () => {
   return (
     <>
-      <Image src={Robot1} alt="Photo of robot" row="firstRow"/>
-      <Image src={Robot2} alt="Photo of robots in suits" row="lastRow"/>
+      <Image iconSrc={ComputerHeadIcon} iconAlt="Head with computer links icon" imageSrc={Robot1} imageAlt="Photo of robot" row="firstRow"/>
+      <Image imageSrc={Robot2} imageAlt="Photo of robots in suits" row="lastRow"/>
     </>
   )
 }
--- src/components/Main/components/Innovation/components/ImageContent/components/Image.jsx
+++ src/components/Main/components/Innovation/components/ImageContent/components/Image.jsx
@@ -5,17 +5,21 @@ import './styles/Image.css'
 const Image = (props) => {
   return (
     <section className='image'>
-        <section className='icon'></section>
+        <section className='iconBg'>
+          <img src={props.iconSrc} alt={props.iconAlt} className='icon'/>
+        </section>
         <section className={props.row} />
-        <img src={props.src} alt={props.alt} />
+        <img src={props.imageSrc} alt={props.imageAlt} className='image'/>
     </section>
   )
 }
 
 Image.propTypes = {
+    iconSrc: PropTypes.string.isRequired,
+    iconAlt: PropTypes.string.isRequired,
     row: PropTypes.string.isRequired,
-    src: PropTypes.string.isRequired,
-    alt: PropTypes.string.isRequired
+    imageSrc: PropTypes.string.isRequired,
+    imageAlt: PropTypes.string.isRequired
 }
 
 export default Image
