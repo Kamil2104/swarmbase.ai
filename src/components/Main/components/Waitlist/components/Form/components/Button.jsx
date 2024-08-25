0d3056a190c226551d2415ef5cc6a58daf46ff2a
pindrive <kamilniewiadomski18@gmail.com>
2024-08-25 02:17:07 +0200

    style: improve stylization of sign up button

--- src/components/Main/components/Waitlist/components/Form/components/Button.jsx
+++ src/components/Main/components/Waitlist/components/Form/components/Button.jsx
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignIn } from '@fortawesome/free-solid-svg-icons';
+
 import './styles/Button.css';
 
 const Button = () => {
   return (
-    <button> REGISTER NOW </button>
+    <button> <FontAwesomeIcon icon={faSignIn} className='signInIcon'/> REGISTER NOW </button>
   )
 }
 
