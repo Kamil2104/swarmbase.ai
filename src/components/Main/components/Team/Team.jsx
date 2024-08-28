ba29d5b7ede217e143ca408b631e723ed80364e5
pindrive <kamilniewiadomski18@gmail.com>
2024-08-29 00:49:36 +0200

    finish stylization of Cards in Team component

--- src/components/Main/components/Team/Team.jsx
+++ src/components/Main/components/Team/Team.jsx
@@ -4,7 +4,6 @@ import Card from './components/Card';
 import Eric from './images/EricPanter.jpeg'
 import Greg from './images/GregBorkowski.png'
 import Joseph from './images/JosephTran.jpeg'
-import You from './images/You.png'
 
 import './styles/Team.css';
 
@@ -13,14 +12,9 @@ const Team = () => {
     <section className='team'>
         <Heading />
         <section className='cards'>
-          <section className='cardsRow'>
-            <Card photo={Eric} name="Eric Panter" linkedin="https://www.linkedin.com/in/%F0%9F%A6%BE-eryk-panter-913038140/" occupation="AI Architect" />
-            <Card photo={Greg} name="Greg Borkowski" linkedin="https://www.linkedin.com/in/gregai/" occupation="Business / Project Manager" />
-          </section>
-          <section className='cardsRow'>
-            <Card photo={Joseph} name="Joseph Tran" linkedin="https://www.linkedin.com/in/jozef-tran/" occupation="AI Developer" />
-            <Card photo={You} name="You" linkedin="none" occupation="We are actively looking for new people. If you are interested feel free to contact us to discuss the details!" />
-          </section>
+          <Card photo={Eric} name="Eric Panter" linkedin="https://www.linkedin.com/in/%F0%9F%A6%BE-eryk-panter-913038140/" occupation="Founder & AI Architect" />
+          <Card photo={Greg} name="Greg Borkowski" linkedin="https://www.linkedin.com/in/gregai/" occupation="Business & Project Manager" />
+          <Card photo={Joseph} name="PhD. Joseph Tran" linkedin="https://www.linkedin.com/in/jozef-tran/" occupation="AI Developer" />
         </section>
     </section>
   )
