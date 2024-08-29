9285d1ef8f75a03e3ab97c88549aa849e6fff696
pindrive <kamilniewiadomski18@gmail.com>
2024-08-29 14:27:26 +0200

    style: make stylization for PrivacyPolicy component

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -2,8 +2,15 @@ import './styles/PrivacyPolicy.css';
 
 const PrivacyPolicy = () => {
   return (
-    <section className='privacyPolicy'>
-        PrivacyPolicy
+    <section className='policy'>
+        <section className='privacyPolicy'>
+          <h1> 1. Privacy policy </h1>
+          <p> Some policies </p>
+        </section>
+        <section className='cookiesPolicy'>
+          <h1> 2. Cookies policy </h1>
+          <p> Some policies </p>
+        </section>
     </section>
   )
 }
