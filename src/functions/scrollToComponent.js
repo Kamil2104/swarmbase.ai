64ee10f28b135432b4d8d25fd9b46a5bd40906db
pindrive <kamilniewiadomski18@gmail.com>
2024-08-31 00:51:11 +0200

    feat: set the offset of scrolling to the component to smaller

--- src/functions/scrollToComponent.js
+++ src/functions/scrollToComponent.js
@@ -1,6 +1,6 @@
 export function scrollToComponent(componentId) {
     let component = document.getElementById(componentId);
-    const offset = -30;
+    const offset = -10;
 
     if (component) {
         const desiredPosition = component.getBoundingClientRect().top + window.scrollY + offset;
