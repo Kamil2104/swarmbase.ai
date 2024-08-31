5ad305b5e8bb906387118a2089beb484226bed43
pindrive <kamilniewiadomski18@gmail.com>
2024-08-31 15:38:41 +0200

    style: create responsiveness for Hero component

--- src/components/Main/components/Hero/Hero.jsx
+++ src/components/Main/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import BackgroundImage from './components/BackgroundImage/BackgroundImage.jsx'
 import ContentBlock from './components/ContentBlock/ContentBlock.jsx'
 
 import './styles/Hero.css'
+import './styles/HeroResponsiveness.css'
 
 const Hero = () => {
   return (
